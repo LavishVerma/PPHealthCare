fd4bc2950235353e909fff6615bb5e210ed1d62e
LavishVerma <lavishverma97@gmail.com>
2021-02-26 21:56:53 +0530

    Eureka Server up and running

---
+++ PPHealthCareSpringBoot/User_Enrollment/src/main/java/com/example/User_Enrollment/UserEnrollmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.User_Enrollment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserEnrollmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserEnrollmentApplication.class, args);
+	}
+
+}
